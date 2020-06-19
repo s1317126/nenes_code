d099e7ca0ea5c2d39469408c71e9921528b3e5b3
s1317126 <s13317126@cst.otsuma.ac.jp>
2020-06-19 13:15:58 +0900

    第四回

---
+++ php04/demo_sql.sql
@@ -0,0 +1,5 @@
+INSERT INTO gs_an_table(id,name,email,age,naiyou,indate)VALUES(NULL,'test1', 'test1@test.jp', 30, 'test', sysdate());
+
+INSERT INTO gs_an_table(id,name,email,age,naiyou,indate)VALUES(NULL,'test2', 'test2@test.jp', 40, 'test', sysdate());
+
+INSERT INTO gs_an_table(id,name,email,age,naiyou,indate)VALUES(NULL,'test3', 'test3@test.jp', 20, 'test', sysdate());
